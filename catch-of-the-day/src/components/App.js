6cc934c014922ce3d089b08f6335efd3866f9696
ryan3738 <ryan3738@gmail.com>
2020-02-19 14:22:46 -0800

    Created header component

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
 
 class App extends React.Component {
   render() {
     return (
       <div className="catch-of-the-day">
-        <p>Heeeey!</p>
+        <div className="menu">
+          <Header />
+        </div>
+        <Order />
+        <Inventory />
       </div>
     );
   }
