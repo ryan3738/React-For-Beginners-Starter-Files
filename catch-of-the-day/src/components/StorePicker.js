81ca191a47be1598f4f41375c18fe3f64bcac903
ryan3738 <ryan3738@gmail.com>
2020-02-20 14:53:32 -0800

    Updating store picker component

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,13 +3,19 @@ import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
   myInput = React.createRef();
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
 
   goToStore = event => {
     //1. Stop the form from submitting
     event.preventDefault();
     //2. Get the text from that input
-    console.log(this.myInput.value.value);
+    console.log(this.myInput.value);
+    // const storeName = this.myInput.value.value;
     //3. Change the page to /store/whatever-they-entered
+    // this.props.history.push(`/store/${storeName}`);
   };
   render() {
     return (
