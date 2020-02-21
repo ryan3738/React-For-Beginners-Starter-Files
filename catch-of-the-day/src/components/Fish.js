2732b1c6dcd0357fcb02b667eea56242a06df1bd
ryan3738 <ryan3738@gmail.com>
2020-02-20 18:03:55 -0800

    Adding sample fish data

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -2,8 +2,12 @@ import React from "react";
 
 class Fish extends React.Component {
   render() {
-    return;
-    <div className="single-fish"></div>;
+    return (
+      <li className="menu-fish">
+        <img src={this.props.details.image} alt={this.props.details.name} />
+        <h3 className="fish-name">{this.props.details.name}</h3>
+      </li>
+    );
   }
 }
 
