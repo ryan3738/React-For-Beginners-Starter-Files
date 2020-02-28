b997fddd65ad93dd4f8a5352262c38aa6fa9ad11
ryan3738 <ryan3738@gmail.com>
2020-02-27 18:35:20 -0800

    Created order variable and update order

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -20,9 +20,20 @@ class App extends React.Component {
     // this.state.fishes.push(fish);
     // this.state.fishes.fish1 = fish;
   };
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
+  addToOrder = key => {
+    //1. take a copy of state
+    const order = { ...this.state.order };
+
+    //2. Either add to the order or update the number in our order
+    order[key] = order[key] + 1 || 1;
+    //3. call set setState to update our state object
+    this.setState({ order });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -30,11 +41,16 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
             ))}
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { formatPrice } from "../helpers";
 
 class Fish extends React.Component {
+  //Added inline
+  // handleClick = () => {
+  //   this.props.addToOrder(this.props.index);
+  // };
   render() {
+    const { image, name, price, desc, status } = this.props.details;
+    const isAvailable = status === "available";
     return (
       <li className="menu-fish">
-        <img src={this.props.details.image} alt={this.props.details.name} />
-        <h3 className="fish-name">{this.props.details.name}</h3>
+        <img src={image} alt={name} />
+        <h3 className="fish-name">
+          {name}
+          <span className="price">{formatPrice(price)}</span>
+        </h3>
+        <p>{desc}</p>
+        <button
+          disabled={!isAvailable}
+          onClick={() => this.props.addToOrder(this.props.index)}
+        >
+          {isAvailable ? "Add To Order" : "Sold Out!"}
+        </button>
       </li>
     );
   }
