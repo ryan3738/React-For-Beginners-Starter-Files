f7b89d41c82e4b08ebb0b56c16ef675f6035c357
ryan3738 <ryan3738@gmail.com>
2020-03-02 11:48:14 -0800

    Added propTypes data validation

--- catch-of-the-day/src/components/EditFishForm.js
+++ catch-of-the-day/src/components/EditFishForm.js
@@ -1,7 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 //This is how you want to handle inputs where state is in the input
 class EditFishForm extends React.Component {
+  static propTypes = {
+    fish: PropTypes.shape({
+      image: PropTypes.string,
+      name: PropTypes.string,
+      desc: PropTypes.string,
+      status: PropTypes.string,
+      price: PropTypes.number
+    }),
+    index: PropTypes.string,
+    updateFish: PropTypes.func
+  };
   handleChange = event => {
     console.log(event.currentTarget.value);
     //update that fish
