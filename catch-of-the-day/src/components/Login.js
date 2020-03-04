22bf2c0d467728692c532ac1dae68f1fb02055c7
ryan3738 <ryan3738@gmail.com>
2020-03-04 10:15:28 -0800

    Finshed Login & logout components

--- catch-of-the-day/src/components/Login.js
+++ catch-of-the-day/src/components/Login.js
@@ -1,26 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Login = () => (
+const Login = props => (
   <nav className="login">
     <h2>Inventory login</h2>
     <p>Sign in to manage your store's inventory.</p>
-    <button
-      className="github"
-      onClick={() => this.props.authenticate("Github")}
-    >
+    <button className="github" onClick={() => props.authenticate("Github")}>
       Log In With GitHub
     </button>
-    <button
-      className="twitter"
-      onClick={() => this.props.authenticate("Twitter")}
-    >
+    <button className="twitter" onClick={() => props.authenticate("Twitter")}>
       Log In With Twitter
     </button>
-    <button
-      className="facebook"
-      onClick={() => this.props.authenticate("Facebook")}
-    >
+    <button className="facebook" onClick={() => props.authenticate("Facebook")}>
       Log In With Facebook
     </button>
   </nav>
